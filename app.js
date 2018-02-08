2cc2ee498cc345d2c6c312ebb5972e54b1a0d36e
gunkol <gunnark13@gmail.com>
2018-02-08 09:38:27 +0000

    bæta við villumeðhöndlun

--- app.js
+++ app.js
@@ -10,6 +10,22 @@ app.set('view engine', 'ejs');
 
 app.use('/', router);
 
+function notFoundHandler(req, res, next) { // eslint-disable-line
+  const title = 'Finnst ekki';
+  const message = 'Efnið sem beðið var um finnst ekki';
+  res.status(404).render('error', { title, message });
+}
+
+function errorHandler(err, req, res, next) { // eslint-disable-line
+  console.error(err);
+  const title = 'Villa kom upp';
+  const message = '';
+  res.status(500).render('error', { title, message });
+}
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
